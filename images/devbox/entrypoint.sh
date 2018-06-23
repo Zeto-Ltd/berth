7fc9655dfc6a6e5cadee03e23dad408f4f39928c
Shane Breatnach <shane.breatnach@gmail.com>
2018-06-23 21:26:32 +0100

    Modifications to devbox image.

---
+++ images/devbox/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# so we can run tests:
+# browsers (even headless ones) needs a display to run hence the virtual display
+export DISPLAY=':99'
+Xvfb ${DISPLAY} -screen 0 1280x1024x24 &
+
+exec "$@"
