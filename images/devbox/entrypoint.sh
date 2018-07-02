2447240a669f071a759820ceaf216ea2dd5f15ed
Shane Breatnach <shane.breatnach@gmail.com>
2018-07-02 07:12:28 +0100

    Keycloak modifications. More changes for devbox venv

--- images/devbox/entrypoint.sh
+++ images/devbox/entrypoint.sh
@@ -3,6 +3,9 @@
 # so we can run tests:
 # browsers (even headless ones) needs a display to run hence the virtual display
 export DISPLAY=':99'
-Xvfb ${DISPLAY} -screen 0 1280x1024x24 &
+if [ ! -f "/tmp/.X99-lock" ]
+then
+    Xvfb ${DISPLAY} -screen 0 1280x1024x24 &
+fi
 
 exec "$@"
