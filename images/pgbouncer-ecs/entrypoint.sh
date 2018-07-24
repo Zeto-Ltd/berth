f0a971f1d2e79aeb9f77760b8988c53bd3af4247
Shane Breatnach <shane@zeto.ie>
2018-07-24 09:07:48 +0100

    Initial version of PgBouncer for ECS

---
+++ images/pgbouncer-ecs/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PG_RUN=/var/run/pgbouncer/
+PG_LOG=/var/log/pgbouncer/
+PG_CONFIG=/etc/pgbouncer/pgbouncer.ini
+PG_USER=postgres
+
+if [ ! -z "${CONFIG_FILE}" ]
+then
+    aws s3 cp ${CONFIG_FILE} ${PG_CONFIG}
+fi
+
+mkdir -p ${PG_RUN}
+mkdir -p ${PG_LOG}
+chown ${PG_USER}:${PG_USER} ${PG_LOG}
+chown ${PG_USER}:${PG_USER} ${PG_RUN}
+
+echo "Starting pgbouncer..."
+exec pgbouncer -q -u ${PG_USER} ${PG_CONFIG}
