5555ea76f85131ee7a1f47b4259b19d54f4cc9ab
Shane Breatnach <shane@zeto.ie>
2018-08-03 16:35:49 +0100

    More updates

--- images/devbox/entrypoint.sh
+++ images/devbox/entrypoint.sh
@@ -7,5 +7,15 @@ if [ ! -f "/tmp/.X99-lock" ]
 then
     Xvfb ${DISPLAY} -screen 0 1280x1024x24 &
 fi
+/etc/init.d/ssh start
 
-exec "$@"
+if [ ! -z "${DEVBOX_USER_UID}" ]
+then
+    usermod -u ${DEVBOX_USER_UID} dev
+fi
+if [ ! -z "${DEVBOX_USER_GID}" ]
+then
+    groupmod -g ${DEVBOX_USER_GID} dev
+fi
+chown -R dev:dev /home/dev
+exec su -c "$@" dev
