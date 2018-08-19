c21046ea0f829b095cb20feb9736a9e00204e86a
Shane Breatnach <shane@zeto.ie>
2018-08-19 18:30:05 +0100

    Supported binding of address for server

--- images/frp/entrypoint.sh
+++ images/frp/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-BIND_ADDRESS=$(ip -o -4 addr list ${BIND_INTERFACE} | head -n1 | awk '{print $4}' | cut -d/ -f1)
-if [ ! -z "${AWS_ENVIRONMENT}" ] && [ ! -z "${CONFIG_FILE}" ]
+if [ ! -z "${CONFIG_FILE}" ]
 then
+    BIND_ADDRESS=$(ip -o -4 addr list ${BIND_INTERFACE} | head -n1 | awk '{print $4}' | cut -d/ -f1)
     aws s3 cp ${CONFIG_FILE} /etc/frp/frps.ini
+    # Since CLI args don't override config file settings, must override bind address in file
+    sed -i -e "s/bind_addr = 0.0.0.0/bind_addr = ${BIND_ADDRESS}/g" /etc/frp/frps.ini
 fi
 
-exec /usr/bin/frps --bind_addr ${BIND_ADDRESS} $*
+exec /usr/bin/frps $*
