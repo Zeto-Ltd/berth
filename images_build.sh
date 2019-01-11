4e02e059143daeb4e2bd24284ba0ed00460af57f
Shane Breatnach <shane@zeto.ie>
2019-01-11 16:35:28 +0000

    Minor modifications to use Python3 for website CI builds.

--- images_build.sh
+++ images_build.sh
@@ -23,8 +23,8 @@ docker push zetoltd/circleci-android-sdk:1.0.0
 docker build -t zetoltd/circleci-cordova-android:1.0.0 ./images/cordova-android/
 docker push zetoltd/circleci-cordova-android:1.0.0
 
-docker build -t zetoltd/circleci-website:1.0.0 ./images/website/
-docker push zetoltd/circleci-website:1.0.0
+docker build -t zetoltd/circleci-website:1.1.0 ./images/website/
+docker push zetoltd/circleci-website:1.1.0
 
 docker build -t zetoltd/circleci-java:1.0.1 ./images/java/
 docker push zetoltd/circleci-java:1.0.1
